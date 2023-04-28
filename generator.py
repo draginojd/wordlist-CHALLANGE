e96ab6e2aa1a884e1dc3304f6f922a5fb97ec536
draginojd <arminfazlikhan@gmail.com>
2023-04-28 18:23:18 +0200

    import time, print(total time, time_passed)

--- generator.py
+++ generator.py
@@ -1,8 +1,14 @@
-
+import time
 
 with open("suffled_words_wordlist.txt", "r") as file:
     contents = file.read()
     shuffled_words = contents.split()
 file.close()
 
+
+start_time = time.time()
 print(shuffled_words)
+end_time = time.time()
+time_passed = end_time - start_time
+
+print("Total time: ", time_passed)
